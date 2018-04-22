8618dfa8e9ba2f57491cef114be6531ac4300a5a
Keith Moss <keithamoss@gmail.com>
2018-04-22 10:41:11 +0800

    Support paging through CKAN now that there are more than 1000 datasets

--- lib/ckan.py
+++ lib/ckan.py
@@ -10,39 +10,52 @@ def get_public_slip_shapefiles():
     """
     Foobar.
     """
+    def get_all_ckan_packages():
+        limit = 1000
+        offset = 0
+        packagesRemain = True
+        packages = []
 
-    url = "https://catalogue.data.wa.gov.au/api/3/action/current_package_list_with_resources?limit=1100"
-    response = requests.get(url, verify=False)
+        while packagesRemain is True:
+            url = "https://catalogue.data.wa.gov.au/api/3/action/current_package_list_with_resources?limit={limit}&offset={offset}".format(limit=limit, offset=offset)
+            response = requests.get(url, verify=False)
 
-    if response.status_code == 200:
-        datasets = []
-        packages = response.json()["result"]
+            if response.status_code == 200:
+                result = response.json()["result"]
+                packages += result
 
-        # This may mean we need to handle paging
-        if len(packages) >= 1000:
-            logger.critical("Got {} packages from CKAN.".format(len(packages)))
+                if len(result) < limit:
+                    packagesRemain = False
+                else:
+                    offset += limit
+            else:
+                raise Exception("Received a '%s' response for the URL %s" % (response.status_code, url))
+        return packages
 
-        for package in packages:
-            dataset = {
-                "name": package["name"],
-                "title": package["title"],
-                "organisation_name": package["organization"]["title"],
-                "resource_urls": [],
-            }
+    datasets = []
+    packages = get_all_ckan_packages()
 
-            slipDataSnapshotResources = [r for r in package["resources"] if r["format"] == "ZIP" and "maps.slip.wa.gov.au/datadownloads/" in r["url"]]
+    logger.info("Got {} packages from CKAN.".format(len(packages)))
 
-            for resource in slipDataSnapshotResources:
-                if resource["url"].endswith(".csv.zip") or resource["url"].endswith(".geojson.zip") or resource["url"].endswith(".gpkg.zip"):
-                    logger.critical("Found a CSV/GeoJSON/GeoPackage dataset with format 'ZIP'")
-                    continue
+    for package in packages:
+        dataset = {
+            "name": package["name"],
+            "title": package["title"],
+            "organisation_name": package["organization"]["title"],
+            "resource_urls": [],
+        }
 
-                if "SLIP_Public_Services" in resource["url"] or "_Public_Secure_" in resource["url"]:
-                    dataset["resource_urls"].append(resource["url"])
+        slipDataSnapshotResources = [r for r in package["resources"] if r["format"] == "ZIP" and "maps.slip.wa.gov.au/datadownloads/" in r["url"]]
 
-            if len(dataset["resource_urls"]) > 0:
-                datasets.append(dataset)
+        for resource in slipDataSnapshotResources:
+            if resource["url"].endswith(".csv.zip") or resource["url"].endswith(".geojson.zip") or resource["url"].endswith(".gpkg.zip"):
+                logger.critical("Found a CSV/GeoJSON/GeoPackage dataset with format 'ZIP'")
+                continue
 
-        return datasets
-    else:
-        raise Exception("Received a '%s' response for the URL %s" % (response.status_code, url))
+            if "SLIP_Public_Services" in resource["url"] or "_Public_Secure_" in resource["url"]:
+                dataset["resource_urls"].append(resource["url"])
+
+        if len(dataset["resource_urls"]) > 0:
+            datasets.append(dataset)
+
+    return datasets
